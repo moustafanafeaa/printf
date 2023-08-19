43f3a32523e7aaafebf1cee284245e52ad83c190
NadaZaki23 <nada02zaki@gmail.com>
2023-08-19 05:26:15 -0700

    header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int put_char(char c);
+int putstr(char *c);
+
+#endif
